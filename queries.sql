494367e4938edd93919339a5674edc3f68d00a93
Olawale <olapetanwale@outlook.com>
2022-08-10 16:10:22 +0100

    Add new queries to the queries.sql file

--- queries.sql
+++ queries.sql
@@ -9,3 +9,10 @@ SELECT name, escape_attemps FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name = 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+SELECT COUNT(name) FROM animals;
+SELECT COUNT(escape_attempts) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT name FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals);
+SELECT species, MAX(weight_kg) AS Max_weight, MIN(weight_kg) AS Min_weight FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) AS AVG_escape_attempts FROM animals WHERE 
+    date_of_birth >= '1990-01-01' AND date_of_birth < '2001-01-01' GROUP BY species;
