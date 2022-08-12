9aed7a2dbed1b4a7f0a5b9890a92e8f8c79834b3
Olawale <olapetanwale@outlook.com>
2022-08-12 08:23:15 +0100

    Add data for the owners and species table

--- queries.sql
+++ queries.sql
@@ -1,18 +1,12 @@
-/*Queries that provide answers to the questions from all projects.*/
+/* Database schema to keep the structure of entire database. */
 
-SELECT * from animals WHERE name = 'Luna';
-SELECT * from animals WHERE name LIKE '%mon%';
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
-SELECT name FROM animals WHERE neutered = true AND escape_attemps < 3;
-SELECT date_of_birth from animals WHERE name IN ('Agumon', 'Pikachu');
-SELECT name, escape_attemps FROM animals WHERE weight_kg > 10.5;
-SELECT * FROM animals WHERE neutered = true;
-SELECT * FROM animals WHERE name = 'Gabumon';
-SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
-SELECT COUNT(name) FROM animals;
-SELECT COUNT(escape_attempts) FROM animals WHERE escape_attempts = 0;
-SELECT AVG(weight_kg) FROM animals;
-SELECT name FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals);
-SELECT species, MAX(weight_kg) AS Max_weight, MIN(weight_kg) AS Min_weight FROM animals GROUP BY species;
-SELECT species, AVG(escape_attempts) AS AVG_escape_attempts FROM animals WHERE 
-    date_of_birth >= '1990-01-01' AND date_of_birth < '2001-01-01' GROUP BY species;
+CREATE TABLE animals (
+    id int GENERATED ALWAYS AS IDENTITY,
+    name varchar(100),
+    date_of_birth date,
+    escape_attempt int,
+    neutered boolean,
+    weight_kg dec,
+    primary key(id),
+    species char(45)
+);
