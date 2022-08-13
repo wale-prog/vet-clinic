f84139cefa4470c96eeccfdb67b81c819a06d98d
Olawale <olapetanwale@outlook.com>
2022-08-13 10:46:47 +0100

    Add data, schema and queries

--- data.sql
+++ data.sql
@@ -28,3 +28,15 @@ UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
 UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
 UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
 UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+
+INSERT INTO specializations(species_id, vet_id)
+VALUES(1, 1), (2, 3), (1, 3), (2, 4);
+
+INSERT INTO visits(animal_id, vet_id, date_of_visit)
+VALUES(15, 1, '2020-05-24'), (15, 3, '2020-07-22'), (16, 4, '2021-02-02'),
+(17, 2, '2020-01-05'), (17, 2, '2020-03-08'), (17, 2, '2020-05-14'),
+(18, 3, '2021-05-04'), (19, 4, '2021-02-24'), (20, 2, '2019-12-21'),
+(20, 1, '2020-08-10'), (20, 2, '2021-04-07'), (21, 3, '2019-09-29'),
+(22, 4, '2020-10-03'), (22, 4, '2020-11-04'), (23, 2, '2019-01-24'),
+(23, 2, '2019-05-15'), (23, 2, '2020-02-27'), (23, 2, '2020-08-03'),
+(24, 3, '2020-05-24'), (24, 1, '2021-01-11');
